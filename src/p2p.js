4a2957ccb0ead9cf5093019ef27f97cce00905d7
Larson Carter <larsoncarter72404@gmail.com>
2024-08-28 13:14:38 -0500

    Start Signaling Logic
    I started working on the p2p logic of the system.

---
+++ src/p2p.js
@@ -0,0 +1,46 @@
+const WebSocket = require('ws');
+
+const peers = {}; // Store connected peers
+
+function setupSignalingServer(server) {
+    const wss = new WebSocket.Server({ server });
+
+    wss.on('connection', (ws) => {
+        const peerId = generatePeerId();
+        peers[peerId] = ws;
+        ws.send(JSON.stringify({ type: 'welcome', peerId }));
+
+        ws.on('message', (message) => {
+            const parsedMessage = JSON.parse(message);
+            handleSignalingMessage(peerId, parsedMessage, ws);
+        });
+
+        ws.on('close', () => {
+            delete peers[peerId];
+        });
+    });
+}
+
+function handleSignalingMessage(peerId, message, ws) {
+    switch (message.type) {
+        case 'offer':
+        case 'answer':
+        case 'candidate':
+            // Relay offer/answer/candidate to the target peer
+            if (message.targetPeerId && peers[message.targetPeerId]) {
+                peers[message.targetPeerId].send(JSON.stringify({
+                    ...message,
+                    fromPeerId: peerId,
+                }));
+            }
+            break;
+        default:
+            console.log('Unknown message type:', message.type);
+    }
+}
+
+function generatePeerId() {
+    return Math.random().toString(36).substring(2, 15);
+}
+
+module.exports = { setupSignalingServer };
